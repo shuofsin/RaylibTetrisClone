ee2eea61f4f1940a9d51b2bfd46912ae70c9e28f
shuofsin <sinhah239@gmail.com>
2026-02-10 19:14:46 -0700

    Blocks be falling

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,28 @@
 #include "raylib.h"
+#include <cstdlib>
 
 #define WIN_WIDTH 600
 #define WIN_HEIGHT 800
 #define BLOCK_SIZE 50
 
-
-typedef enum DIRECTION {UP, DOWN, RIGHT, LEFT} Direction;
-
 typedef struct Block {
-    int x_pos;
-    int y_pos; 
+    int x;
+    int y; 
+    bool active; 
 } Block;
 
-typedef struct Tetromino {
-    Block blocks[4];
-    Direction direction; 
-} Tetromino;
+Block blocks[10][16];
 
-typedef struct I {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (3 * BLOCK_SIZE), y_pos}};
-    Color color = WHITE;
-} I;
-
-typedef struct O {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos, y_pos + (BLOCK_SIZE)}, {x_pos + (BLOCK_SIZE), y_pos + (BLOCK_SIZE)}};
-    Color color = WHITE;
-} O;
-
-typedef struct T {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos + (BLOCK_SIZE)}};
-    Color color = WHITE;
-} T;
-
-typedef struct J {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (3 * BLOCK_SIZE), y_pos}};
-    Color color = WHITE;
-} J;
-
-typedef struct L {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (3 * BLOCK_SIZE), y_pos}};
-    Color color = WHITE;
-} L;
-
-typedef struct S {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (3 * BLOCK_SIZE), y_pos}};
-    Color color = WHITE;
-} S;
-
-typedef struct Z {
-    int x_pos;
-    int y_pos; 
-    Direction direction = UP; 
-    Block blocks[4] = {{x_pos, y_pos}, {x_pos + BLOCK_SIZE, y_pos}, {x_pos + (2 * BLOCK_SIZE), y_pos}, {x_pos + (3 * BLOCK_SIZE), y_pos}};
-    Color color = WHITE;
-} Z;
-
+float timer = 0;
+float gravity_time = 0.2; 
 
 int main(int argc, char const *argv[]) {
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 16; j++) {
+            blocks[i][j] = {i + 1, j, false};
+        }
+    }
 
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "Tetris");
     SetTargetFPS(60);
@@ -85,6 +31,39 @@ int main(int argc, char const *argv[]) {
         BeginDrawing();
         ClearBackground(BLACK);
 
+        timer += GetFrameTime();
+
+        if (timer > gravity_time) {
+            timer = 0; 
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 15; j++) {
+                    if (blocks[i][j].active == true && blocks[i][j + 1].active == false) {
+                        blocks[i][j + 1].active = true; 
+                        blocks[i][j].active = false; 
+                        break; 
+                    }
+                }
+            }
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            int x = GetMouseX() / BLOCK_SIZE;
+            int y = GetMouseY() / BLOCK_SIZE;
+            if (x >= 1 && x < 11 && y >= 0 && y < 16) {
+                blocks[x - 1][y].active = !blocks[x - 1][y].active;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 16; j++) {
+                DrawRectangleLines(blocks[i][j].x * BLOCK_SIZE, blocks[i][j].y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, WHITE);
+                if (blocks[i][j].active) {
+                    DrawRectangle(blocks[i][j].x * BLOCK_SIZE, blocks[i][j].y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, WHITE);
+                }
+            }
+        }
+
+        
 
         EndDrawing();
     }
